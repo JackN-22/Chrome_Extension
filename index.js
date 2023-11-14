f10944d8f35b38162e1c60dffff2bb79f918511f
JackN-22 <145039268+JackN-22@users.noreply.github.com>
2023-11-14 21:37:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const textEl = document.getElementById("text-el")
 const resultsEl = document.getElementById("results-el")
 const saveEl = document.getElementById("save-el")
 const resetEl = document.getElementById("reset-el")
+const tabBtn = document.getElementById("tab-el")
 let resultsString = []
 
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("resultsString")) 
@@ -16,6 +17,16 @@ saveEl.addEventListener("click", function(){
     textEl.value = ""
     localStorage.setItem("resultsString", JSON.stringify(resultsString))
     renderResults()
+})
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id
+        resultsString.push(tabs[0].url)
+        localStorage.setItem("resultsString", JSON.stringify(resultsString))
+        renderResults()
+      });
 })
 
 resetEl.addEventListener("click", function(){
